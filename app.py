154846600a2a9498409d942c4decc59333c3d66c
gunjkarpushkar <gunjkarpushkar8@gmail.com>
2024-04-09 19:09:04 +0530

    first-commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template,request
 import pickle
 import numpy as np
+import pandas as pd
 
 pbr_df = pickle.load(open('PopularBookRecommendation.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
 book = pickle.load(open('book.pkl','rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+
+dt = pd.read_csv("data/data.csv", on_bad_lines = 'skip')
 
 app = Flask(__name__)
 
@@ -17,7 +20,8 @@ def index():
                            publisher=list(pbr_df['Publisher'].values),
                            image = list(pbr_df['Image-URL-M'].values),
                            votes = list(pbr_df['Num_rating'].values),
-                           rating = list(pbr_df['Avg_rating'].values))
+                           rating = list(pbr_df['Avg_rating'].values),
+                           categories = list(dt['categories'].values))
 
 @app.route('/recommendation')
 def recommendation_ui():
@@ -40,6 +44,30 @@ def recommendation():
         data.append(item)
     print(data)
     return render_template('recommendation.html',data=data)
+
+
+@app.route('/categories')
+def categories():
+    return render_template('categories.html')
+
+
+@app.route('/recommend_categories', methods=['POST'])
+def recommend_categories():
+    categories_name = request.form.get('categories_name')  # Safely access form data using get method
+    if categories_name is None:
+        return "Category name not provided."
+    
+    filtered_books = dt[dt['categories'] == categories_name]
+    if not filtered_books.empty:
+        sorted_books = filtered_books.sort_values(by='average_rating', ascending=False)
+        if len(sorted_books) >= 10:
+            return sorted_books.head(5).to_html()
+        else:
+            return sorted_books.to_html()
+    else:
+        return "No recommendations available for this category."
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
